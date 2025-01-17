b8248cdc5afa92718c7ed7ac422b5d84353a6a4b
Chi-Kuan Chiu <devaraja1945@gmail.com>
2025-01-17 17:31:19 +0800

    Add share_array
    This version is capable of storing red-black tree information,
    allowing it to be accessed by other processes.
    
    A ring buffer is now used to manage indices of `record_item`
    within the shared array. The ring buffer maintains indices in
    FIFO order. Its size must match the shared array.
    
    The `test_shared` function simulates 10,000 read and write
    operations on a shared array of size 128 bytes.

--- ringbuf-shm/ringbuffer.c
+++ ringbuf-shm/ringbuffer.c
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define RINGBUF_PAD(SIZE) (((size_t)(SIZE) + 7U) & (~7U))
@@ -16,12 +18,26 @@ typedef struct {
     uint32_t size, gap;
 } ringbuf_element_t;
 
+typedef struct record_item{
+    const char* op_name;
+    uint64_t Memory_usage;
+    uint64_t op_time;
+}record_item;
+
+uint64_t get_time()
+{
+    struct timespec ts;
+    clock_gettime(0, &ts);
+    return (uint64_t)(ts.tv_sec * 1e6 + ts.tv_nsec / 1e3);
+}
+
 typedef struct {
     size_t size, mask, rsvd /* reserved */, gapd;
     memory_order acquire, release;
     atomic_size_t head, tail;
     uint8_t buf[] __attribute__((aligned(sizeof(ringbuf_element_t))));
 } ringbuf_t;
+
 
 static inline size_t ringbuf_body_size(size_t minimum)
 {
@@ -277,27 +293,44 @@ static const struct timespec req = {.tv_sec = 0, .tv_nsec = 1};
 static uint64_t iterations = 10000;
 #define THRESHOLD (RAND_MAX / 256)
 #define PAD(SIZE) (((size_t)(SIZE) + 7U) & (~7U))
+#define ARRAY_LENGTH 128
+char* options[] = {"Add tree", "Add node", "Delete tree", "Delete node"};
 
 static void *producer_main(void *arg)
 {
     ringbuf_t *ringbuf = arg;
-
+    int shm_fd;
+    record_item *shared_array;
+    shm_fd = shm_open("/shm_array", O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open failed");
+        exit(1);
+    }
+    if (ftruncate(shm_fd, sizeof(record_item) * ARRAY_LENGTH) == -1) {
+        perror("ftruncate failed");
+        shm_unlink("/shm_array");
+        exit(1);
+    }
+    shared_array = mmap(NULL, sizeof(record_item) * ARRAY_LENGTH, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shared_array == MAP_FAILED) {
+        perror("mmap failed");
+        shm_unlink("/shm_array");
+        exit(1);
+    }
+    
     uint64_t cnt = 0;
     while (cnt < iterations) {
         if (rand() < THRESHOLD)
             nanosleep(&req, NULL);
-
-        size_t written = PAD(rand() * 1024.f / RAND_MAX);
-
+        size_t written = PAD(sizeof(uint64_t));
         size_t maximum;
-        uint8_t *ptr;
+        uint64_t *ptr;
         if ((ptr = ringbuf_write_request_max(ringbuf, written, &maximum))) {
-            assert(maximum >= written);
-            const uint8_t *end = ptr + written;
-            for (uint8_t *src = ptr; src < end; src += sizeof(uint64_t)) {
-                *(uint64_t *) src = cnt;
-                assert(*(uint64_t *) src == cnt);
-            }
+            *ptr = cnt%ARRAY_LENGTH;
+            //save record_item
+            shared_array[*ptr].op_name = options[rand()%4];
+            shared_array[*ptr].op_time = get_time();
+            shared_array[*ptr].Memory_usage = rand()%500;
             ringbuf_write_advance(ringbuf, written);
             cnt++;
         } /* else: buffer full */
@@ -309,23 +342,43 @@ static void *producer_main(void *arg)
 static void *consumer_main(void *arg)
 {
     ringbuf_t *ringbuf = arg;
+    int shm_fd;
+    record_item *shared_array;
+    shm_fd = shm_open("/shm_array", O_RDONLY, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open failed");
+        exit(1);
+    }
+     shared_array = mmap(NULL, sizeof(record_item) * ARRAY_LENGTH, PROT_READ, MAP_SHARED, shm_fd, 0);
+    if (shared_array == MAP_FAILED) {
+        perror("mmap failed");
+        close(shm_fd);
+        exit(1);
+    }
 
     uint64_t cnt = 0;
+    uint64_t prev = 0;
     while (cnt < iterations) {
         if (rand() < THRESHOLD)
             nanosleep(&req, NULL);
 
-        const uint8_t *ptr;
+        const uint64_t *ptr;
         size_t toread;
         if ((ptr = ringbuf_read_request(ringbuf, &toread))) {
-            const uint8_t *end = ptr + toread;
-            for (const uint8_t *src = ptr; src < end; src += sizeof(uint64_t))
-                assert(*(const uint64_t *) src == cnt);
+            if(shared_array[*ptr].op_time < prev){
+                printf("Saving error");
+                munmap(shared_array, sizeof(record_item) * ARRAY_LENGTH);
+                close(shm_fd);
+                shm_unlink("/shm_array");
+                exit(1);
+            }
+            prev = shared_array[*ptr].op_time;
+            printf("Action : %s, op_time : %lld, Memory_usage :%lld \n",shared_array[*ptr].op_name, shared_array[*ptr].op_time, shared_array[*ptr].Memory_usage);
             ringbuf_read_advance(ringbuf);
             cnt++;
         } /* else: buffer empty */
     }
-
+   
     return NULL;
 }
 
@@ -411,14 +464,14 @@ static void test_shared()
     const char *name = "/ringbuf_shm_test";
     if (pid == 0) { /* child process */
         ringbuf_shm_t ringbuf_shm;
-        assert(ringbuf_shm_init(&ringbuf_shm, name, 8192, true) == 0);
+        assert(ringbuf_shm_init(&ringbuf_shm, name, 1024, true) == 0);
 
         consumer_main(ringbuf_shm.ringbuf);
 
         ringbuf_shm_deinit(&ringbuf_shm);
     } else { /* parent process */
         ringbuf_shm_t ringbuf_shm;
-        assert(ringbuf_shm_init(&ringbuf_shm, name, 8192, true) == 0);
+        assert(ringbuf_shm_init(&ringbuf_shm, name, 1024, true) == 0);
 
         producer_main(ringbuf_shm.ringbuf);
 
@@ -430,8 +483,13 @@ int main(int argc, char **argv)
 {
     srand(time(NULL));
 
-    test_threaded();
+    //test_threaded();
     test_shared();
-
+    int shm_fd;
+    record_item *shared_array;
+    shm_fd = shm_open("/shm_array", O_RDONLY, 0666);
+    munmap(shared_array, sizeof(record_item) * ARRAY_LENGTH);
+    close(shm_fd);
+    shm_unlink("/shm_array");
     return 0;
 }
